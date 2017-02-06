4ca8f2c3c9ace95cb4a55f783dd2a3db73d1e099
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2017-02-06 20:04:54 +0900

    implement line button mock

---
+++ docs/line-button/js/script.js
@@ -0,0 +1,25 @@
+document.addEventListener( "DOMContentLoaded", function() {
+  var sourceShare = '<div class="line-it-button" data-lang="ja" data-type="share-【デザイン左から順にa〜eの文字】" data-url="https://media.line.me/ja/how_to_install.html" style="display: none;"></div>';
+  var sourceFriend = '<div class="line-it-button" data-lang="ja" data-type="friend" data-lineid="【@で始まるLINE ID】" data-count="（友だち数を表示する場合）true" data-home="（ホームへの移動ボタンを表示する場合）true" style="display: none;"></div>';
+  var sourceLike = '<div class="line-it-button" data-lang="ja" data-type="like" data-url="【いいねするページのURL】" data-share="（いいねと共にシェアする場合）true" data-lineid="（友だち追加ボタンのオプションありの場合）@lineteamjp" style="display: none;"></div>';
+
+  var textareaHTML = '<textarea class="elm-textarea" cols="80" rows="5"></textarea>'
+
+  document.querySelectorAll('.share-source').forEach(function(elm, id) {
+    var source = elm.getAttribute('data-source');
+    var textareaElm;
+
+    elm.innerHTML = textareaHTML;
+    textareaElm = elm.querySelector('textarea');
+
+    if(source === 'share') {
+      textareaElm.textContent = sourceShare;
+    }
+    if(source === 'friend') {
+      textareaElm.textContent = sourceFriend;
+    }
+    if(source === 'like') {
+      textareaElm.textContent = sourceLike;
+    }
+  });
+});
