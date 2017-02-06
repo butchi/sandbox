ad6688551d2479c5dd564f78fb7d807574105c34
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2017-02-07 08:23:20 +0900

    fix share url and line id

--- docs/line-button/js/script.js
+++ docs/line-button/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener( "DOMContentLoaded", function() {
-  var sourceShare = '<div class="line-it-button" data-lang="ja" data-type="【デザイン左から順にshare-a〜share-eの文字列】" data-url="https://media.line.me/ja/how_to_install.html" style="display: none;"></div>';
+  var sourceShare = '<div class="line-it-button" data-lang="ja" data-type="【デザイン左から順にshare-a〜share-eの文字列】" data-url="【シェアするURL】" style="display: none;"></div>';
   var sourceFriend = '<div class="line-it-button" data-lang="ja" data-type="friend" data-lineid="【@で始まるLINE ID】" data-count="（友だち数を表示する場合）true" data-home="（ホームへの移動ボタンを表示する場合）true" style="display: none;"></div>';
   var sourceLike = '<div class="line-it-button" data-lang="ja" data-type="like" data-url="【いいねするページのURL】" data-share="（いいねと共にシェアする場合）true" data-lineid="（友だち追加ボタンのオプションありの場合）@lineteamjp" style="display: none;"></div>';
 
