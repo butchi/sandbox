521e1507b827b9795779563b04bedcd6cfe4a83a
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2017-02-07 09:00:43 +0900

    add classic button source

--- docs/line-button/js/script.js
+++ docs/line-button/js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener( "DOMContentLoaded", function() {
+  var sourceClassic = '<a href="http://line.me/R/msg/text/?【送る文字列をパーセントエンコーディング（例: http%3A%2F%2F）で指定】"><img src="【画像のパス】" width="【画像の横幅】" height="【画像の縦幅】" alt="LINEで送る" /></a>'
   var sourceShare = '<div class="line-it-button" data-lang="ja" data-type="【デザイン左から順にshare-a〜share-eの文字列】" data-url="【シェアするURL】" style="display: none;"></div>';
   var sourceFriend = '<div class="line-it-button" data-lang="ja" data-type="friend" data-lineid="【@で始まるLINE ID】" data-count="（友だち数を表示する場合）true" data-home="（ホームへの移動ボタンを表示する場合）true" style="display: none;"></div>';
   var sourceLike = '<div class="line-it-button" data-lang="ja" data-type="like" data-url="【いいねするページのURL】" data-share="（いいねと共にシェアする場合）true" data-lineid="【友だち追加ボタンのオプションありの場合@で始まるLINE ID】@lineteamjp" style="display: none;"></div>';
@@ -12,6 +13,9 @@ document.addEventListener( "DOMContentLoaded", function() {
     elm.innerHTML = textareaHTML;
     textareaElm = elm.querySelector('textarea');
 
+    if(source === 'classic') {
+      textareaElm.textContent = sourceClassic;
+    }
     if(source === 'share') {
       textareaElm.textContent = sourceShare;
     }
